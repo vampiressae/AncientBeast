1e39f635e3acb62fd0def1b615f30ac8db439e83
Nehmulos <Nehmulos@inbox.com>
2012-06-14 08:44:01 +0200

    added onReady callback, not fully used yet. It will be used to check if all the textures and data is loaded

--- combat/Drawable.js
+++ combat/Drawable.js
@@ -1,2 +1,19 @@
 function Drawable() {
+    this.onReadyCallback = null;
+    this.loadingCounter = 0;
+    this.loadingTarget  = 0;
+}
+
+
+// call when all textures are loaded and the Creature is ready to be displayed
+Drawable.prototype.onReady = function() {
+    if (this.onReadyCallback) {
+        this.onReadyCallback();
+    }
+}
+
+Drawable.prototype.loadingProgress = function() {
+    if (this.loadingCounter >= this.loadingTarget) {
+        this.onReady();
+    }
 }
